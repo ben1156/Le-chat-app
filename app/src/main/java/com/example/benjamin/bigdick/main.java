d3d2de80aad270bc8c2c433e24195fde6df95f31
ben1156 <burlesonbenjamin@gmail.com>
2017-06-02 15:15:11 -0500

    JonGabeBenCahtApp
    basic of what i did so far.

---
+++ app/src/main/java/com/example/benjamin/bigdick/main.java
@@ -0,0 +1,17 @@
+package com.example.benjamin.bigdick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.Log
+
+public class main extends AppCompatActivity {
+    public static final  String MY_TAG ="Le JBG chat";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log. i(MY_TAG,"onCreate"):
+    }
+}
